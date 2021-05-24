6e373b813f37a482366e2082b47bc58fd2b7ec14
unknown <sahanikrishna0000@gmail.com>
2021-05-24 11:27:04 +0530

    added more files on binary tree

--- lbcs/Binary_Tree/diagonal_traversal.cpp
+++ lbcs/Binary_Tree/diagonal_traversal.cpp
@@ -1,88 +1,171 @@
-/*
 
-Diagonal Traversal of Binary Tree
-Difficulty Level : Medium
-Last Updated : 08 Feb, 2021
-Consider lines of slope -1 passing between nodes. Given a Binary Tree, print all diagonal elements in a binary tree belonging to the same line.
 
-Input : Root of below tree
-for image of tree visit this page
-https://www.geeksforgeeks.org/diagonal-traversal-of-binary-tree/   
+//recusionsion solution
 
-Output : 
-Diagonal Traversal of binary tree : 
- 8 10 14
- 3 6 7 13
- 1 4
+// #include <iostream>
+// #include <vector>
+// #include <unordered_map>
+// using namespace std;
+ 
+// // Data structure to store a binary tree node
+// struct Node
+// {
+//     int data;
+//     Node *left, *right;
+ 
+//     Node(int data)
+//     {
+//         this->data = data;
+//         this->left = this->right = nullptr;
+//     }
+// };
+ 
+// // Recursive function to perform preorder traversal on the tree and
+// // fill the map with diagonal elements
+// void printDiagonal(Node* node, int diagonal, auto &map)
+// {
+//     // base case: empty tree
+//     if (node == nullptr) {
+//         return;
+//     }
+ 
+//     // insert the current node into the current diagonal
+//     map[diagonal].push_back(node->data);
+ 
+//     // recur for the left subtree by increasing diagonal by 1
+//     printDiagonal(node->left, diagonal + 1, map);
+ 
+//     // recur for the right subtree with the same diagonal
+//     printDiagonal(node->right, diagonal, map);
+// }
+ 
+// // Function to print the diagonal elements of a given binary tree
+// void printDiagonal(Node* root)
+// {
+//     // create an empty map to store the diagonal element in every slope
+//     unordered_map<int, vector<int>> map;
+ 
+//     // perform preorder traversal on the tree and fill the map
+//     printDiagonal(root, 0, map);
+ 
+//     // traverse the map and print the diagonal elements
+//     int temp = 0;
+ 
+//     for (int i = 0; i < map.size(); i++)
+//     {
+//         for (int i: map[i]) {
+//             cout << i << ' ';
+//         }
+//         cout << endl;
+//     }
+// }
+ 
+// int main()
+// {
+//     /* Construct the following tree
+//               1
+//             /   \
+//            /     \
+//           2       3
+//          /      /  \
+//         /      /    \
+//        4      5      6
+//              / \
+//             /   \
+//            7     8
+//     */
+ 
+//     Node* root = new Node(1);
+//     root->left = new Node(2);
+//     root->right = new Node(3);
+//     root->left->left = new Node(4);
+//     root->right->left = new Node(5);
+//     root->right->right = new Node(6);
+//     root->right->left->left = new Node(7);
+//     root->right->left->right = new Node(8);
+ 
+//     printDiagonal(root);
+ 
+//     return 0;
+// }
+//iterative solution
 
-*/
 
-#include<bits/stdc++.h>
+#include <iostream>
+#include <queue>
 using namespace std;
 
-typedef struct Node{
-    int data;
-    Node *left,*right;
-}Node;
-
-Node* createTree(int n){
-    Node *root=new Node();
-    root->data=n;
-    root->left=root->right=NULL;
-    return root;
-}
-
-
-//Function to return a list containing elements of left view of the binary tree.
-//basic idea is  to give no if left then increament of cureewnt value and if right then move right with same number
-vector<int> diagonalTraversal(Node *root)
+// Data structure to store a binary tree node
+struct Node
 {
-   // Your code here
-   vector<int>v;
+	int data;
+	Node *left, *right;
+
+	Node(int data)
+	{
+		this->data = data;
+		this->left = this->right = nullptr;
+	}
+};
+
+// Iterative function to print the diagonal elements of a given binary tree
+vector<int> diagonal(Node *root)
+{
+   // your code here
+    vector<int>v;
    if(!root){
        return v;
        
    }
    
    Node *temp;
-   queue<pair<Node*,int>>q;
-   q.push({root,0});
-   map<int,vector<int>>m;
+   queue<Node*>q;
+   q.push(root);
    
+   //idea is to move till possible and if any node have its child node push it into queue and 
+//    update temp with right node if exist
    while(!q.empty()){
-        temp=q.front().first;
-        int h=q.front().second;
-        q.pop();
-        m[h].push_back(temp->data);
-        if(temp->left){
-            q.push({temp->left,h+1});
-        }
-        if(temp->right){
-            q.push({temp->right,h});
-
-        }
-   }
-   for(auto it:m){
-       v.insert(v.end(),it.second.begin(),it.second.end());
+       temp=q.front();
+       q.pop();
+       while(temp){
+           if(temp->left){
+               q.push(temp->left);
+           }
+           v.push_back(temp->data);
+           temp=temp->right;
+       }
    }
    return v;
-   
 }
 
+int main()
+{
+	/* Construct the following tree
+			   1
+			 /   \
+			/     \
+		  2        3
+		 /        /  \
+		/        /    \
+	   4        5      6
+			   / \
+			 /    \
+			7      8
+	*/
 
+	Node* root = new Node(1);
+	root->left = new Node(2);
+	root->right = new Node(3);
+	root->left->left = new Node(4);
+	root->right->left = new Node(5);
+	root->right->right = new Node(6);
+	root->right->left->left = new Node(7);
+	root->right->left->right = new Node(8);
 
-int main(){
-    Node*root=createTree(1);
-    root->left=createTree(2);
-    root->right=createTree(3);
-    root->left->left=createTree(4);
-    root->left->right=createTree(5);
-    root->right->left=createTree(6);
-    root->right->right=createTree(7);
-    root->left->left->right=createTree(8);
-    vector<int>ans=diagonalTraversal(root);
+    vector<int>ans=	diagonal(root);
     for(int i=0;i<ans.size();i++){
         cout<<ans[i]<<" ";
-    }cout<<endl;
-    return 0;
+    } cout<<endl;
+
+	return 0;
 }
